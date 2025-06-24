8b915dbb401a4a955e9f303edc73b35010a85d0b
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-06-24 14:32:04 +0600

    Update roulette.js

--- commands/economy/roulette.js
+++ commands/economy/roulette.js
@@ -1,17 +1,14 @@
 // commands/economy/roulette.js
 const { SlashCommandBuilder } = require('discord.js');
+const { startRouletteGame } = require('../../utils/rouletteManager'); // We will create this new manager file
 
 module.exports = {
     category: 'economy',
     data: new SlashCommandBuilder()
         .setName('roulette')
-        .setDescription('Starts a game of multiplayer roulette in this channel.'),
+        .setDescription('Starts a multiplayer game of roulette.'),
 
-    async execute(interaction) {
-        // The core logic is now handled by the interactionCreate event handler
-        // to manage the game state. This command just needs to trigger it.
-        // We will call a function from the interactionCreate logic here.
-        const { startRouletteGame } = require('../../events/interactionHandlers'); // We will create this file next
-        await startRouletteGame(interaction);
+    async execute(interaction, db) {
+        await startRouletteGame(interaction, db);
     },
 };
