5bf71973fddcac4f02d2bdfbcc9fd468ed44e1de
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-06-24 13:38:47 +0600

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,41 +1,27 @@
 // events/interactionCreate.js
-const { Events, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
-const { createErrorEmbed, createSuccessEmbed, createGheeEmbed } = require('../utils/embeds');
-const { FieldValue } = require('firebase-admin/firestore');
-const Blackjack = require('../utils/blackjack');
-const Roulette = require('../utils/roulette');
-const { activeGames } = require('../commands/economy/blackjack');
+const { Events } = require('discord.js');
 
-// Handler functions defined here...
-async function handleCommand(interaction, db) { /* ... */ }
-async function handleProposalButton(interaction, db) { /* ... */ }
-async function handleBlackjackButton(interaction, db) { /* ... */ }
-async function handleLewdConsent(interaction, db) { /* ... */ }
-async function handleAgeVerification(interaction, db) { /* ... */ }
-async function handleRouletteButton(interaction, db) { /* ... */ }
-async function handleRouletteModal(interaction, db) { /* ... */ }
-async function runRouletteSpin(interaction, db) { /* ... */ }
+async function handleCommand(interaction, db) {
+    console.log(`[DIAGNOSTIC] ROUTER: handleCommand started for /${interaction.commandName}.`);
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`[DIAGNOSTIC] ROUTER: No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+    try {
+        await command.execute(interaction, db);
+        console.log(`[DIAGNOSTIC] ROUTER: Finished command: /${interaction.commandName}.`);
+    } catch (error) {
+        console.error(`[DIAGNOSTIC] ROUTER: Error executing /${interaction.commandName}:`, error);
+    }
+}
 
-// --- MAIN INTERACTION ROUTER ---
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction, db) {
         if (interaction.isChatInputCommand()) {
+            console.log(`[DIAGNOSTIC] ROUTER: Interaction received. User: ${interaction.user.username}, Command: /${interaction.commandName}`);
             await handleCommand(interaction, db);
-        } else if (interaction.isButton()) {
-            const [context] = interaction.customId.split('_');
-            if (context === 'roulette') await handleRouletteButton(interaction, db);
-            else if (context === 'accept' || context === 'decline') await handleProposalButton(interaction, db);
-            else if (context === 'blackjack') await handleBlackjackButton(interaction, db);
-            else if (context === 'consent') await handleLewdConsent(interaction, db);
-            else if (context === 'age') await handleAgeVerification(interaction, db);
-        } else if (interaction.isModalSubmit()) {
-            const [context] = interaction.customId.split('_');
-            if (context === 'roulette') await handleRouletteModal(interaction, db);
-        } else if (interaction.isAutocomplete()) {
-            const command = interaction.client.commands.get(interaction.commandName);
-            if (!command || !command.autocomplete) return;
-            try { await command.autocomplete(interaction, db); } catch (e) { console.error(e); }
         }
     },
 };
