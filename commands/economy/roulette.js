de48d5bf01601a937e4509ac5f03ace97f61ba5c
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-06-24 14:55:13 +0600

    Update roulette.js

--- commands/economy/roulette.js
+++ commands/economy/roulette.js
@@ -1,14 +1,15 @@
 // commands/economy/roulette.js
 const { SlashCommandBuilder } = require('discord.js');
-const { startRouletteGame } = require('../../utils/rouletteManager'); // We will create this new manager file
+const { startRouletteGame } = require('../../utils/rouletteManager');
 
 module.exports = {
     category: 'economy',
     data: new SlashCommandBuilder()
         .setName('roulette')
-        .setDescription('Starts a multiplayer game of roulette.'),
+        .setDescription('Starts a multiplayer game of roulette in this channel.'),
 
     async execute(interaction, db) {
+        // The new manager handles everything
         await startRouletteGame(interaction, db);
     },
 };
