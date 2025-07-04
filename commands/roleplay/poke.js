da26a371b82004eb7f15a32d766804478f042845
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-04 23:39:43 +0600

    Update poke.js

--- commands/roleplay/poke.js
+++ commands/roleplay/poke.js
@@ -1,18 +1,25 @@
 // commands/roleplay/poke.js
 const { SlashCommandBuilder } = require('discord.js');
 const { createGheeEmbed } = require('../../utils/embeds');
+const { getGif } = require('../../utils/gifFetcher');
 
 module.exports = {
+    category: 'roleplay',
     data: new SlashCommandBuilder()
         .setName('poke')
         .setDescription('Annoy someone with a poke.')
         .addUserOption(option => option.setName('user').setDescription('The person you want to poke.').setRequired(true)),
 
     async execute(interaction) {
+        await interaction.deferReply();
         const user = interaction.user;
         const target = interaction.options.getUser('user');
-        const embed = createGheeEmbed('Hey! Listen!', `${user} pokes ${target}. Annoying, yet effective.`)
-            .setImage('https://i.imgur.com/H41G2iH.gif');
-        await interaction.reply({ embeds: [embed] });
+        
+        const gifUrl = await getGif('anime poke');
+        
+        const embed = createGheeEmbed('Hey! Listen!', `${user} pokes ${target} to get their attention.`)
+            .setImage(gifUrl);
+            
+        await interaction.editReply({ embeds: [embed] });
     },
 };
