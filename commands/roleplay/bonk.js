36f113d29bb0ae827f2d00138f3ce607152c49b0
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-04 23:37:47 +0600

    Update bonk.js

--- commands/roleplay/bonk.js
+++ commands/roleplay/bonk.js
@@ -1,18 +1,25 @@
 // commands/roleplay/bonk.js
 const { SlashCommandBuilder } = require('discord.js');
 const { createGheeEmbed } = require('../../utils/embeds');
+const { getGif } = require('../../utils/gifFetcher');
 
 module.exports = {
+    category: 'roleplay',
     data: new SlashCommandBuilder()
         .setName('bonk')
-        .setDescription('Bonk someone for being naughty.')
-        .addUserOption(option => option.setName('user').setDescription('The person you want to bonk.').setRequired(true)),
+        .setDescription('Send someone to horny jail.')
+        .addUserOption(option => option.setName('user').setDescription('The person to bonk.').setRequired(true)),
 
     async execute(interaction) {
+        await interaction.deferReply();
         const user = interaction.user;
         const target = interaction.options.getUser('user');
-        const embed = createGheeEmbed('Go to Horny Jail!', `${user} bonks ${target} right on the head!`)
-            .setImage('https://i.imgur.com/w4p4zBs.gif');
-        await interaction.reply({ embeds: [embed] });
+        
+        const gifUrl = await getGif('anime bonk');
+        
+        const embed = createGheeEmbed('BONK!', `${user} sends ${target} to horny jail.`)
+            .setImage(gifUrl);
+            
+        await interaction.editReply({ embeds: [embed] });
     },
 };
