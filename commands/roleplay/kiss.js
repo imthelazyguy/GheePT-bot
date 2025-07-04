cabe23560ade4388a212d1eddbb8066c74f2634f
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-04 23:38:24 +0600

    Update kiss.js

--- commands/roleplay/kiss.js
+++ commands/roleplay/kiss.js
@@ -1,18 +1,25 @@
 // commands/roleplay/kiss.js
 const { SlashCommandBuilder } = require('discord.js');
 const { createGheeEmbed } = require('../../utils/embeds');
+const { getGif } = require('../../utils/gifFetcher');
 
 module.exports = {
+    category: 'roleplay',
     data: new SlashCommandBuilder()
         .setName('kiss')
         .setDescription('Give someone a little smooch.')
         .addUserOption(option => option.setName('user').setDescription('The person you want to kiss.').setRequired(true)),
 
     async execute(interaction) {
+        await interaction.deferReply();
         const user = interaction.user;
         const target = interaction.options.getUser('user');
-        const embed = createGheeEmbed('Mwah!', `${user} gives ${target} a sweet kiss!`)
-            .setImage('https://i.imgur.com/s4b4t5C.gif');
-        await interaction.reply({ embeds: [embed] });
+        
+        const gifUrl = await getGif('anime kiss');
+        
+        const embed = createGheeEmbed('😘 Smooch!', `${user} gives ${target} a sweet kiss!`)
+            .setImage(gifUrl);
+            
+        await interaction.editReply({ embeds: [embed] });
     },
 };
