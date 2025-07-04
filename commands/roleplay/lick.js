e9e15df40e2d0f4239de91776d36a1f27aa0505a
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-04 23:40:21 +0600

    Update lick.js

--- commands/roleplay/lick.js
+++ commands/roleplay/lick.js
@@ -1,30 +1,25 @@
 // commands/roleplay/lick.js
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { createGheeEmbed, createErrorEmbed } = require('../../utils/embeds');
-const { FieldValue } = require('firebase-admin/firestore');
-const { logModerationAction } = require('../../utils/moderation');
-
-async function runShame(interaction, config) {
-    // ... (This function is identical to the one in tease.js)
-}
+const { SlashCommandBuilder } = require('discord.js');
+const { createGheeEmbed } = require('../../utils/embeds');
+const { getGif } = require('../../utils/gifFetcher');
 
 module.exports = {
+    category: 'roleplay',
     data: new SlashCommandBuilder()
         .setName('lick')
-        .setDescription('This is a taste of... Ghee? (18+)')
-        .addUserOption(option => option.setName('user').setDescription('The user to lick.').setRequired(true)),
+        .setDescription('This is the taste of a liar, Giorno Giovanna!')
+        .addUserOption(option => option.setName('user').setDescription('The person you want to... lick?').setRequired(true)),
 
-    async execute(interaction, db) {
-        // This command's safety check logic is IDENTICAL to tease.js
-        // It checks for the shame condition, the lewd channel, and user consent.
-        // If all checks pass, it executes the command logic below.
-
-        // --- Command Logic ---
-        await db.collection('users').doc(`${interaction.guild.id}-${interaction.user.id}`).update({ lewdCommandCount: FieldValue.increment(1) });
+    async execute(interaction) {
+        await interaction.deferReply();
         const user = interaction.user;
         const target = interaction.options.getUser('user');
-        const embed = createGheeEmbed('A questionable taste...', `${user} licks ${target}... a bold and confusing move.`)
-            .setImage('https://i.imgur.com/nJm4L23.gif');
-        await interaction.reply({ embeds: [embed] });
+        
+        const gifUrl = await getGif('anime lick');
+        
+        const embed = createGheeEmbed('Uhh...', `${user} licks ${target}. Gross.`)
+            .setImage(gifUrl);
+            
+        await interaction.editReply({ embeds: [embed] });
     },
 };
