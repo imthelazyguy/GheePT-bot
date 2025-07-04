171e00d246400b6c8253bb1ea308a1a2744ecaa7
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-05 00:50:41 +0600

    Update roulette.js

--- commands/economy/roulette.js
+++ commands/economy/roulette.js
@@ -1,16 +1,16 @@
 // commands/economy/roulette.js
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { createErrorEmbed } = require('../../utils/embeds');
 
 module.exports = {
     category: 'economy',
     data: new SlashCommandBuilder()
         .setName('roulette')
-        .setDescription('Starts a multiplayer game of roulette.'),
+        .setDescription('Starts a multiplayer game of roulette in this channel.'),
 
     async execute(interaction, db) {
         const channelId = interaction.channel.id;
-        // Access the global memory from the client object
+        // The game state is now stored on the central client object
         if (interaction.client.activeRouletteGames.has(channelId)) {
             return interaction.reply({ embeds: [createErrorEmbed("A roulette game is already in progress in this channel!")], ephemeral: true });
         }
@@ -31,20 +31,18 @@ module.exports = {
 
         const message = await interaction.reply({ embeds: [embed], components: [row], fetchReply: true });
 
+        // This function is now part of interactionCreate.js
+        const { spinRouletteWheel } = require('../../events/interactionCreate');
+
         const game = {
             hostId: interaction.user.id,
             players: new Map(),
             state: 'betting',
             message: message,
             db: db,
-            timeout: setTimeout(() => {
-                // We need to pass the client object to the spin function
-                const { spinWheel } = require('../../events/interactionHandlers');
-                spinWheel(interaction.client, channelId);
-            }, 60 * 1000),
+            timeout: setTimeout(() => spinRouletteWheel(interaction.client, channelId), 60 * 1000),
         };
         
-        // Save the game to the central client memory
         interaction.client.activeRouletteGames.set(channelId, game);
     },
 };
