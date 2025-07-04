5f4af5ce3fd297f5aad2b6a4a630a0780d72951b
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-04 23:32:56 +0600

    Update hug.js

--- commands/roleplay/hug.js
+++ commands/roleplay/hug.js
@@ -1,18 +1,25 @@
 // commands/roleplay/hug.js
 const { SlashCommandBuilder } = require('discord.js');
 const { createGheeEmbed } = require('../../utils/embeds');
+const { getGif } = require('../../utils/gifFetcher');
 
 module.exports = {
+    category: 'roleplay',
     data: new SlashCommandBuilder()
         .setName('hug')
         .setDescription('Give someone a warm hug.')
         .addUserOption(option => option.setName('user').setDescription('The person you want to hug.').setRequired(true)),
 
     async execute(interaction) {
+        await interaction.deferReply();
         const user = interaction.user;
         const target = interaction.options.getUser('user');
+        
+        const gifUrl = await getGif('anime hug');
+        
         const embed = createGheeEmbed('Aww, how sweet!', `${user} gives ${target} a big, warm hug!`)
-            .setImage('https://i.imgur.com/r9aU2xv.gif'); // Example GIF
-        await interaction.reply({ embeds: [embed] });
+            .setImage(gifUrl);
+            
+        await interaction.editReply({ embeds: [embed] });
     },
 };
