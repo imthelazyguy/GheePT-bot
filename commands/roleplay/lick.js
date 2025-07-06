437bee3977faba37b40c728b331d23c4cad6fdc5
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-06 22:06:03 +0600

    Update lick.js

--- commands/roleplay/lick.js
+++ commands/roleplay/lick.js
@@ -1,4 +1,3 @@
-// commands/roleplay/lick.js
 const { SlashCommandBuilder } = require('discord.js');
 const { createGheeEmbed } = require('../../utils/embeds');
 const { getGif } = require('../../utils/gifFetcher');
@@ -9,17 +8,10 @@ module.exports = {
         .setName('lick')
         .setDescription('This is the taste of a liar, Giorno Giovanna!')
         .addUserOption(option => option.setName('user').setDescription('The person you want to... lick?').setRequired(true)),
-
     async execute(interaction) {
         await interaction.deferReply();
-        const user = interaction.user;
-        const target = interaction.options.getUser('user');
-        
         const gifUrl = await getGif('anime lick');
-        
-        const embed = createGheeEmbed('Uhh...', `${user} licks ${target}. Gross.`)
-            .setImage(gifUrl);
-            
+        const embed = createGheeEmbed('Uhh...', `${interaction.user} licks ${interaction.options.getUser('user')}. Gross.`).setImage(gifUrl);
         await interaction.editReply({ embeds: [embed] });
     },
 };
