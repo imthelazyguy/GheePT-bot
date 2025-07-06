e78e18e23ab16ce59dddac30160b21ecea736e4b
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-06 07:19:13 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 // config.js
 // This file contains all the core configuration values for the bot.
 module.exports = {
+    // Add your Discord User ID here to get special perks in commands like /pp
+    OWNER_IDS: ["1380432991548866585","742975167478628402"],
+
     // XP & Leveling System
     XP_COOLDOWN_SECONDS: 60,
-    XP_PER_MESSAGE_MIN: 1,
-    XP_PER_MESSAGE_MAX: 5,
+    XP_PER_MESSAGE_MIN: 5,
+    XP_PER_MESSAGE_MAX: 15,
     XP_PER_VOICE_MINUTE: 2,
     LEVEL_UP_REWARD_BASE: 100,
 
