598afbe2f3ac721d9beae8cc542e2ba5b42d9f0c
Spidey <103317378+imthelazyguy@users.noreply.github.com>
2025-07-06 22:05:31 +0600

    Update kiss.js

--- commands/roleplay/kiss.js
+++ commands/roleplay/kiss.js
@@ -1,4 +1,3 @@
-// commands/roleplay/kiss.js
 const { SlashCommandBuilder } = require('discord.js');
 const { createGheeEmbed } = require('../../utils/embeds');
 const { getGif } = require('../../utils/gifFetcher');
@@ -9,17 +8,10 @@ module.exports = {
         .setName('kiss')
         .setDescription('Give someone a little smooch.')
         .addUserOption(option => option.setName('user').setDescription('The person you want to kiss.').setRequired(true)),
-
     async execute(interaction) {
         await interaction.deferReply();
-        const user = interaction.user;
-        const target = interaction.options.getUser('user');
-        
         const gifUrl = await getGif('anime kiss');
-        
-        const embed = createGheeEmbed('😘 Smooch!', `${user} gives ${target} a sweet kiss!`)
-            .setImage(gifUrl);
-            
+        const embed = createGheeEmbed('😘 Smooch!', `${interaction.user} gives ${interaction.options.getUser('user')} a sweet kiss!`).setImage(gifUrl);
         await interaction.editReply({ embeds: [embed] });
     },
 };
